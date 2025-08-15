c63e6fa715afe46c704e0d45b0fff883d2ce6667
Muaaz Bayat <76963273+MuaazBayat@users.noreply.github.com>
2025-08-15 19:55:51 +0200

    feature: added globe

--- services/frontend/components/header.tsx
+++ services/frontend/components/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
  export default function Header(){
     return(
          <header className="flex p-4 gap-4 h-16 bg-black items-center justify-between">
-        <div className="text-emerald-50">GlobeTalks</div>
+        <div className="text-xl text-white font-bold tracking-tighter md:text-2xl lg:text-3xl">GlobeTalk</div>
         <div className="flex items-center gap-2">
         <SignedOut>
         <SignInButton>
