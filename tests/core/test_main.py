58e34c4cd0f9ebda75f9ad6156521e6b441aea1a
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-08-30 23:11:49 +0200

    Revert "allow for 0 coverage"
    This reverts commit 6dd1ad12a6de26166ba50faa6772f13c659d5387.

---
+++ tests/core/test_main.py
@@ -0,0 +1,10 @@
+
+from services.core.main import add
+from services.core.models import ProfileCreate, ProfileUpdate, Profile
+def test_add_function():
+    """
+    This test checks if the add function works correctly.
+    """
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
