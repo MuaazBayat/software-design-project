f1fd9fdc870cdd338b2b75872ad583cc7079f635
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-09-01 16:18:39 +0200

    bugfix: fix ability to access conversation and compose letter pages when not signed in

--- services/frontend/middleware.ts
+++ services/frontend/middleware.ts
@@ -3,7 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 // Define which routes require authentication
 const isProtectedRoute = createRouteMatcher([
   // Add your protected routes here
-  '/inbox(.*)'
+  '/inbox(.*)',
+  '/compose-letter(.*)',
+  '/conversation(.*)',
   
   //'/dashboard(.*)',
   //'/profile(.*)',
